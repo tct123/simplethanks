8e4120647d1a918c241a210681606d03c6279da7
tct123 <42028373+tct123@users.noreply.github.com>
2023-09-14 19:34:16 +0200

    added Audiosupport

--- src/simplethanks/app.py
+++ src/simplethanks/app.py
@@ -5,10 +5,16 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
+
 plattform = toga.platform.current_platform
 
 if plattform != "android" and plattform != "ios":
     import playsound
+    from pathlib import Path
+else:
+    from android.media import MediaPlayer
+    from os.path import dirname, join
+
 
 class SimpleThanks(toga.App):
     def startup(self):
@@ -18,14 +24,26 @@ class SimpleThanks(toga.App):
         Usually, you would add your application to a main content box.
         We then create a main window (with a name matching the app), and
         show the main window.
-        """
+        """        
         main_box = toga.Box()
 
         # widgets
-        thxtext = toga.Label(text="Thank you, ...",style=Pack(padding=10,flex=1))
-        birthdaybtn = toga.Button(text="Birthday", on_press=self.pressed_birthdaybtn, style=Pack(padding=10,flex=1))
-        mothersdaybtn = toga.Button(text="Mothers Day", on_press=self.pressed_mothersdaybtn, style=Pack(padding=10,flex=1))
-        fathersdaybtn = toga.Button(text="Fathers day", on_press=self.pressed_fathersdaybtn, style=Pack(padding=10,flex=1))
+        thxtext = toga.Label(text="Thank you, ...", style=Pack(padding=10, flex=1))
+        birthdaybtn = toga.Button(
+            text="Birthday",
+            on_press=self.pressed_birthdaybtn,
+            style=Pack(padding=10, flex=1),
+        )
+        mothersdaybtn = toga.Button(
+            text="Mothers Day",
+            on_press=self.pressed_mothersdaybtn,
+            style=Pack(padding=10, flex=1),
+        )
+        fathersdaybtn = toga.Button(
+            text="Fathers day",
+            on_press=self.pressed_fathersdaybtn,
+            style=Pack(padding=10, flex=1),
+        )
 
         # add
         main_box.add(thxtext)
@@ -38,13 +56,23 @@ class SimpleThanks(toga.App):
         self.main_window.show()
 
     def pressed_birthdaybtn(self, widget):
-        pass
+        if plattform != "android" and plattform != "ios":
+            self.resources_folder = Path(__file__).joinpath("../resources").resolve()
+            self.birthday_filepath = self.resources_folder.joinpath("happy-birthday-whistled.wav")
+            print(self.birthday_filepath)
+            playsound.playsound(sound=self.birthday_filepath)
+        else:
+            player = MediaPlayer()
+            sound =join(dirname(__file__), "resources/happy-birthday-whistled.wav")
+            player.setDataSource(sound)
+            player.prepare()
+            player.start()
 
     def pressed_mothersdaybtn(self, widget):
-        pass
+        self.main_window.info_dialog(title="Happy Mothersday",message="Happy Mothersday")
 
     def pressed_fathersdaybtn(self, widget):
-        pass
+        self.main_window.info_dialog(title="Happy Fathersday",message="Happy Fathersday")
 
 
 def main():
