bcd0cce21dbd6761bef2d707b3227ad81230c5e2
tct123 <42028373+tct123@users.noreply.github.com>
2023-12-13 18:01:04 +0100

    Revert "."
    This reverts commit a25d209778211a3dd1dacf6b7b55c6d97c06697e.

--- snd.py
+++
@@ -1,6 +0,0 @@
-# src/simplethanks/resources/happy-birthday-whistled.wav
-from pydub import AudioSegment
-from pydub.playback import play
-
-song = AudioSegment.from_wav("src/simplethanks/resources/happy-birthday-whistled.wav")
-play(song)
