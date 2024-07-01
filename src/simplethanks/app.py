bc5216a3e4710bd1c4a0ddfc2c040fbfa0200058
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-01 09:42:08 +0200

    Update app.py

--- src/simplethanks/app.py
+++ src/simplethanks/app.py
@@ -29,7 +29,7 @@ class SimpleThanks(toga.App):
             lang = locale.getlocale()
             lang, _ = lang
             self.lang = lang
-        # self._description = tr(csv_file=self.file, target_key="DESCRIBTION", langcode=self.lang)
+        self._description = tr(csv_file=self.file, target_key="DESCRIBTION", langcode=self.lang)
         main_box = toga.Box()
 
         # widgets
